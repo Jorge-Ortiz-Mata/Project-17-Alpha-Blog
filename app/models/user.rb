5be635baec8f15128393698252af5ca3707cba60
Jorge Ortiz <ortiz.mata.jorge@gmail.com>
2022-02-01 20:51:32 -0600

    Models' comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,28 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 2, maximum: 25 }
+
+    # --------------------------------------> ARTICLE Model. <------------------------------------------
+
+    # ------------------------------ CONSTANTS Section. -----------------------------------
+    
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+    # ---------------------------------------------------------------------------------------
+    # ------------------------------ VALIDATIONS  Section. -----------------------------------
+
+    validates :username, presence: true, length: { minimum: 2, maximum: 25 }
     validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
-    has_many :articles, dependent: :destroy
     validates :password, presence: true
     validates :password_confirmation, presence: true
+
+    # ---------------------------------------------------------------------------------------
+    # ------------------------------ SECURE PASSWORD Section. -----------------------------------
+
     has_secure_password
+
+    # ---------------------------------------------------------------------------------------
+    # ------------------------------ ASOCIATIONS Section. -----------------------------------
+
+    has_many :articles, dependent: :destroy
+
+    # ---------------------------------------------------------------------------------------
 end
